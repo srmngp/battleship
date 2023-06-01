c969afc632621f39d181ca7fbefe8e613eec5677
srmngp <sr.mngp@gmail.com>
2023-06-01 12:31:35 +0200

    Create example game utils

---
+++ src/logic/createExampleGame.js
@@ -0,0 +1,55 @@
+import { collection, doc, setDoc } from 'firebase/firestore'
+import { db } from './repository/firebaseDb'
+import { savePlayerNameInLocalStorage } from './localStorageManager'
+
+export const createExampleGame = () => {
+
+  const game = {
+    id: 'test-game',
+    boardSize: 49,
+    fleet: [{ label: ['🚤'], value: 1 }, { label: ['🛥', '🛥', '🛥'], value: 3 }],
+    owner: 'Player 1',
+    status: 'IN_PROGRESS',
+    players: [
+      { name: 'Player 1', color: 'red' },
+      { name: 'Player 2', color: 'blue' }
+    ]
+  }
+
+  saveGame(game)
+  savePlayer(
+    buildNewTestPlayer(1),
+    buildNewTestPlayer(2),
+    buildNewTestPlayer(3))
+  savePlayerNameInLocalStorage('Player-1')
+}
+
+const buildNewTestPlayer = (number) => {
+  return {
+    id: `Player-${number}`,
+    gameId: 'test-game',
+    name: `Player-${number}`,
+    ready: true,
+    avatarUrl: `https://garticphone.com/images/avatar/${number}.svg`,
+    grid: ['🚤', null, null, null, null, null, null, '🛥', '🛥', '🛥', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+    hitsGrid: [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
+  }
+
+}
+
+const saveGame = (game) => {
+
+  const gamesRef = collection(db, 'Games')
+  const testGameRef = doc(gamesRef, game.id)
+
+  return setDoc(testGameRef, game)
+}
+
+const savePlayer = (...players) => {
+  players.forEach(player => {
+    const playersRef = collection(db, 'Games/test-game/Players')
+    const testPlayerRef = doc(playersRef, player.id)
+
+    setDoc(testPlayerRef, player)
+  })
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { createGame } from '../logic/gameService'
 import PlayerCreator from '../components/player/PlayerCreator'
+import { createExampleGame } from '../logic/createExampleGame'
 
 export const Home = () => {
 
@@ -15,6 +16,14 @@ export const Home = () => {
   return (
     <>
       <PlayerCreator createAction={createGameButton} buttonText='Create game' />
+
+      <button onClick={() => {
+        createExampleGame()
+        navigation('/game/test-game')
+      }}
+      >
+        Create example game
+      </button>
     </>
   )
 
